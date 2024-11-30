cd9a944a2591e293ea44136d38de739537695746
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-11-30 10:34:55 +0530

    Add dialogs for load confirmation and load confirmation success

--- src/components/sidebar-buttons/sidebar-buttons.component.tsx
+++ src/components/sidebar-buttons/sidebar-buttons.component.tsx
@@ -1,27 +1,48 @@
 import { Stack, Typography } from '@mui/material'
 import PlusButton from '../../assets/icons/Plus Button.png'
+import LoadConfirmationDialog from '../load-confirmation-dialog/load-confirmation-dialog.component'
+import LoadConfirmationSuccessDialog from '../load-confirmation-dialog/load-confirmation-success-dialog.component'
+import { useState } from 'react'
+import { CreateButton } from './sidebar-buttons.styles'
 
 const SidebarButtons = () => {
-  return (
-    <Stack spacing={2} justifyContent="center" alignItems="center">
-      <Stack alignItems="center" justifyContent="center" textAlign="center">
-        <img src={PlusButton} alt="plus button" width={50} height={50} />
-        <Typography fontWeight={700} color="#fff">
-          Create
-          <br />
-          Order
-        </Typography>
-      </Stack>
+  const [loadConfirmOpen, setLoadConfirmOpen] = useState(false)
+  const [loadSuccessOpen, setLoadSuccessOpen] = useState(false)
 
-      <Stack alignItems="center" textAlign="center">
-        <img src={PlusButton} alt="plus button" width={50} height={50} />
-        <Typography fontWeight={700} color="#fff">
-          Create
-          <br />
-          Trip
-        </Typography>
+  return (
+    <>
+      <Stack spacing={2} justifyContent="center" alignItems="center">
+        <Stack alignItems="center" justifyContent="center" textAlign="center">
+          <CreateButton
+            src={PlusButton}
+            alt="plus button"
+            onClick={() => setLoadConfirmOpen(true)}
+          />
+          <Typography fontWeight={700} color="#fff">
+            Create
+            <br />
+            Order
+          </Typography>
+        </Stack>
+
+        <Stack alignItems="center" textAlign="center">
+          <CreateButton src={PlusButton} alt="plus button" />
+          <Typography fontWeight={700} color="#fff">
+            Create
+            <br />
+            Trip
+          </Typography>
+        </Stack>
       </Stack>
-    </Stack>
+      <LoadConfirmationDialog
+        open={loadConfirmOpen}
+        handleClose={() => setLoadConfirmOpen(false)}
+      />
+      <LoadConfirmationSuccessDialog
+        open={loadSuccessOpen}
+        handleClose={() => setLoadSuccessOpen(false)}
+      />
+    </>
   )
 }
 
---
+++ src/components/sidebar-buttons/sidebar-buttons.styles.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const CreateButton = styled.img`
+  width: 50px;
+  height: 50px;
+  cursor: pointer;
+`
