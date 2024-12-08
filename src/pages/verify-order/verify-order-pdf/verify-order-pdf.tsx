b5fd13e0096621e7d28a306ea022949c1705308e
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-12-08 09:20:07 +0530

    Resize issue fix for default pdf viewer

--- src/pages/verify-order/verify-order-pdf/verify-order-pdf.tsx
+++ src/pages/verify-order/verify-order-pdf/verify-order-pdf.tsx
@@ -1,9 +1,13 @@
+import { useMediaQuery, useTheme } from '@mui/material'
 import React from 'react'
 // import PdfViewer from '../../../components/pdf-viewer/pdf-viewer.component'
 
 const VerifyOrderPdf = () => {
+  const theme = useTheme()
+  const isSm = useMediaQuery(theme.breakpoints.down('sm'))
+
   return (
-    <div style={{ width: '100%', height: '100%' }}>
+    <div style={{ width: 'auto', height: isSm ? '550px' : '100%' }}>
       <iframe
         src={`${process.env.PUBLIC_URL}/LoadConfirmation.pdf`}
         width="100%"
