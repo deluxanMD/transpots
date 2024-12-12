d07c52a9ed062283b5ac4343bac6234eefad8f95
Deluxan Mariathasan <deluxan.mariathasan@gmail.com>
2024-12-12 20:35:04 +0530

    Send invoice email dialog

--- src/pages/invoice/invoice-details/invoice-table/invoice-table-body/invoice-table-actions/invoice-table-actions.tsx
+++ src/pages/invoice/invoice-details/invoice-table/invoice-table-body/invoice-table-actions/invoice-table-actions.tsx
@@ -5,6 +5,8 @@ import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 import { editInvoice } from '../../../../../../store/invoices/invoices.slice'
 import { StyledTableCell } from '../invoice-table-body'
+import InvoiceTableEmailDialog from '../invoice-table-email-dialog/invoice-table-email-dialog'
+import { useState } from 'react'
 
 type InvoiceTableCellProps = {
   disabled?: boolean
@@ -20,43 +22,60 @@ const InvoiceTableActions = ({
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
+  const [dialogOpen, setDialogOpen] = useState(false)
+
+  const handleDialogOpen = () => {
+    setDialogOpen(true)
+  }
+
+  const handleDialogClose = () => {
+    setDialogOpen(false)
+  }
+
   const handleEdit = () => {
     dispatch(editInvoice({ invoiceId }))
     navigate(invoiceId)
   }
 
   return (
-    <StyledTableCell sx={{ minWidth }} align={align}>
-      <Stack direction="row" alignItems="center" spacing={1}>
-        <Button
-          sx={{
-            height: '36px',
-            borderRadius: '6px',
-            textTransform: 'capitalize',
-          }}
-          variant="contained"
-          disabled={disabled}
-        >
-          Send Invoice
-        </Button>
+    <>
+      <StyledTableCell sx={{ minWidth }} align={align}>
+        <Stack direction="row" alignItems="center" spacing={1}>
+          <Button
+            sx={{
+              height: '36px',
+              borderRadius: '6px',
+              textTransform: 'capitalize',
+            }}
+            variant="contained"
+            disabled={disabled}
+            onClick={handleDialogOpen}
+          >
+            Send Invoice
+          </Button>
 
-        <img
-          src={Edit}
-          alt="edit"
-          width={36}
-          height={36}
-          style={{ cursor: 'pointer' }}
-          onClick={handleEdit}
-        />
-        <img
-          src={Delete}
-          alt="delete"
-          width={28}
-          height={32}
-          style={{ cursor: 'pointer' }}
-        />
-      </Stack>
-    </StyledTableCell>
+          <img
+            src={Edit}
+            alt="edit"
+            width={36}
+            height={36}
+            style={{ cursor: 'pointer' }}
+            onClick={handleEdit}
+          />
+          <img
+            src={Delete}
+            alt="delete"
+            width={28}
+            height={32}
+            style={{ cursor: 'pointer' }}
+          />
+        </Stack>
+      </StyledTableCell>
+      <InvoiceTableEmailDialog
+        open={dialogOpen}
+        handleClose={handleDialogClose}
+      />
+    </>
   )
 }
 
